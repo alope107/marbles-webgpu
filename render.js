2800484d97d3b85764f37181588c7f247b51973d
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-07-17 18:47:14 -0400

    Circles of many colors

--- render.js
+++ render.js
@@ -4,10 +4,15 @@ ${circleStruct.code}
 
 const PI = radians(180.0);
 
+struct VertexOutput {
+    @builtin(position) position : vec4f,
+    @location(0) color : vec4f
+}
+
 @group(0) @binding(0) var<storage, read> circles : array<Circle>; 
 
 @vertex fn triangle(@builtin(vertex_index) vertexIdx : u32, 
-                    @builtin(instance_index) instanceIdx : u32) -> @builtin(position) vec4f {
+                    @builtin(instance_index) instanceIdx : u32) -> VertexOutput {
     let circle = circles[instanceIdx];
     let r = select(0., circle.radius, (vertexIdx & 1) == 0); // Alternate between edges and center
     let segments = ${polysPerCircle}.;
@@ -15,10 +20,13 @@ const PI = radians(180.0);
     // TODO precompute matrix
     let angle = 2 * PI * f32(vertexIdx) / (2*segments);
 
-    return vec4f(circle.center + (r * vec2f(cos(angle), sin(angle))), 0, 1.);
+    return VertexOutput(
+        vec4f(circle.center + (r * vec2f(cos(angle), sin(angle))), 0, 1.),
+        circle.color
+    );
 }
 
-@fragment fn solidColor(@builtin(position) position : vec4f) -> @location(0) vec4f {
-    return vec4(1, 0, 0, 1);
+@fragment fn solidColor(fragInput : VertexOutput) -> @location(0) vec4f {
+    return fragInput.color;
 }
 `;
